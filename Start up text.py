8fa75285a4633e7c834dc8e68ab1145e2eaeef4f
GabolinscyA <78507892+GabolinscyA@users.noreply.github.com>
2021-06-17 09:33:55 +1200

    Update Start up text.py
    I added a confirmation screen when selecting the password generator option in the main menu. This means that the user won't accidentally start the generation process.
    To make everything look more streamlined and nice to look at. I added some print("\n") statements through the code so that when running the text is spread out and easy to read

--- Start up text.py
+++ Start up text.py
@@ -1,12 +1,27 @@
+def confirmation_check():
+    print("\n")
+    confirmation = input("You have chosen to start generating a new password. Are you sure you would like to continue this function?")
+    confirmation = confirmation.strip().lower()
+    if confirmation == "yes":
+        generation()
+        options()
+    elif confirmation == "no":
+        print("\n")
+        print("Returning you to the main menu now")
+        options()
+    else:
+        print("\n")
+        print("Please answer with yes or no")
+        confirmation_check()
 def generation():
     print("1234567890")
 def options():
+    print("\n")
     print("You have three options. Would you like to 1:Generate a new password or 2:Look at your current password list or 3:Close this program")
     startup = input("Pick 1, 2, or 3")
     startup = startup.strip().lower()
     if startup == "1":
-       generation()
-       options()
+       confirmation_check()
     elif startup == "2":
         file = open("C:/Users/gabolinscya/Desktop/Passwords.txt", "r")
         lines = file.readlines()
@@ -17,13 +32,9 @@ def options():
         file.close()
         options()
     elif startup == "3":
+        print("\n")
         print("Thank you for using this password generator!")
     else:
         options()
-
-
-
 print("Welcome to your very own password generator!")
 options()
-
-
