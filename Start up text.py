a804b7f26430c78d546167c9ad2b25ff4328b4c6
GabolinscyA <78507892+GabolinscyA@users.noreply.github.com>
2021-06-18 13:12:06 +1200

    Update Start up text.py
    Updated the generation section of the code with the already made code from the main side. This way I can now add new passwords to my text file from this prototype while testing later builds.

--- Start up text.py
+++ Start up text.py
@@ -1,3 +1,14 @@
+def generation():
+    passw0rds = input("Please enter a random number")
+    website = input("Your new password is {}. What would you like this password to be used for?".format(passw0rds))
+    file = open("C:/Users/gabolinscya/Desktop/Passwords.txt", "a")
+    file.write("\n")
+    file.write(website)
+    file.write(":")
+    file.write("\n")
+    file.write(passw0rds)
+    file.close()
+    print("Your password has been saved in the 'Passwords' text file on your desktop")
 def confirmation_check():
     print("\n")
     confirmation = input("You have chosen to start generating a new password. Are you sure you would like to continue this function?")
@@ -12,8 +23,6 @@ def confirmation_check():
     else:
         print("Please answer with yes or no")
         confirmation_check()
-def generation():
-    print("1234567890")
 def options():
     print("\n")
     print("You have three options. Would you like to 1:Generate a new password or 2:Look at your current password list or 3:Close this program")
