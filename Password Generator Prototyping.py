e6e4cee3dc3f6c73453bc216e5e0e4ab9314fb2c
gabolinscya <gabolinscya@stu.otc.school.nz>
2021-08-09 10:32:42 +1200

    This update has put all the password generating code into a for loop. This loop generates the password variables and puts them into a new list (password_list) which after loop finishes wil print off the full list.
    The loop how ever isn't randomized yet. Which means the password is always 6 pattern loops long. I am unhappy with how the password prints as well as I want the characters to be squished together so it's easier to copy and paste

--- Password Generator Prototyping.py
+++ Password Generator Prototyping.py
@@ -1,21 +1,26 @@
 import random
 import secrets
-
 az_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-alphebet_pick = secrets.choice(az_list)
-capitalize_letter = random.getrandbits(1)
-if capitalize_letter == True:
-    alphebet_pick = alphebet_pick.capitalize()
-elif capitalize_letter == False:
-    alphebet_pick = alphebet_pick
-print(alphebet_pick)
-
-number_pick = random.randint(0,9)
-print(number_pick)
-
 symbol_list = ['`', '~', '!', '@', '$', '#', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '[', '}', ']', '|', '<', ',', '>', '.', '?', '/', ':', ';']
-symbol_pick = secrets.choice(symbol_list)
-print(symbol_pick)
+password_list = []
 
-pattern_repeat = random.randint(3,8)
-print(pattern_repeat)
+for i in range(3,9):
+    alphebet_pick = secrets.choice(az_list)
+    capitalize_letter = random.getrandbits(1)
+    if capitalize_letter == True:
+        alphebet_pick = alphebet_pick.capitalize()
+    elif capitalize_letter == False:
+        alphebet_pick = alphebet_pick
+    password_list.append(alphebet_pick)
+    number_pick = random.randint(0,9)
+    password_list.append(number_pick)
+    symbol_pick = secrets.choice(symbol_list)
+    password_list.append(symbol_pick)
+print(password_list)
+
+
+
+
+
+#pattern_repeat = random.randint(3,8)
+#print(pattern_repeat)
