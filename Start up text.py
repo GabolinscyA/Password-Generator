6145274cc5aa4c1a1f01cf8bc2118665293cd615
gabolinscya <gabolinscya@stu.otc.school.nz>
2021-08-10 14:48:24 +1200

    Imported the 'Password Generator Prototying' code into the 'generation' function. Currently the password is not showing when printed from other functions even though I have been carrying the variable over.
    WIll fix at a later date but the fact I have imported the code without the whole program crashing is important

--- Start up text.py
+++ Start up text.py
@@ -1,20 +1,42 @@
-def generation():
-    password_numb = input("Please enter a random number")
-    keep_password_checK(password_numb)
+#Declaring the 'random' and the 'secrets' operation
+import random
+import secrets
+#Creating lists so that the random operation may pick a character within the list
+az_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+symbol_list = ['`', '~', '!', '@', '$', '#', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '[', '}', ']', '|', '<', ',', '>', '.', '?', '/', ':', ';']
+password_list = []
+#Declaring the variable 'pattern_repeat' so that the 'for i in range' function works
+pattern_repeat = random.randint(3,8)
 
-def keep_password_checK(password_numb):
+def generation():
+    for i in range(pattern_repeat):
+        alphebet_pick = secrets.choice(az_list)
+        capitalize_letter = random.getrandbits(1)
+        if capitalize_letter == True:
+            alphebet_pick = alphebet_pick.capitalize()
+        elif capitalize_letter == False:
+            alphebet_pick = alphebet_pick
+        password_list.append(alphebet_pick)
+        number_pick = random.randint(0,9)
+        password_list.append(number_pick)
+        symbol_pick = secrets.choice(symbol_list)
+        password_list.append(symbol_pick)
+    password_numb = print(*password_list, sep="")
+    keep_password_checK(password_numb, password_list)
+
+def keep_password_checK(password_numb, password_list):
     print("\n")
     save_password = input("Your new password is {}. Would you like to save this password?".format(password_numb))
     save_password = save_password.strip().lower()
     if save_password == "yes":
-        password_saving(password_numb)
+        password_saving(password_numb, password_list)
     elif save_password == "no":
-        save_confirmation(password_numb)
+        save_confirmation(password_numb, password_list)
     else:
         print("Please answer 'yes' or 'no'")
-        keep_password_checK(password_numb)
+        keep_password_checK(password_numb, password_list)
 
-def password_saving(password_numb):
+def password_saving(password_numb, password_list):
     website = input("What would you like this password to be used for?")
     file = open("C:/Users/gabolinscya/Desktop/Passwords.txt", "a")
     file.write("\n")
@@ -29,16 +51,16 @@ def password_saving(password_numb):
     print("If you want to view your new password, type '2' in the main menu")
     print("Alternatively, you can view your password directly in the 'Passwords' text file on your desktop")
 
-def save_confirmation(password_numb):
+def save_confirmation(password_numb, password_list):
     save_confirm = input("Are you sure you wouldn't like to save the password:{}?".format(password_numb))
     save_confirm = save_confirm.strip().lower()
     if save_confirm == "yes":
         print("Directing you back to the menu...")
     elif save_confirm == "no":
-        password_saving(password_numb)
+        password_saving(password_numb, password_list)
     else:
         print("Please answer 'yes' or 'no'")
-        save_confirmation(password_numb)
+        save_confirmation(password_numb, password_list)
 
 def confirmation_check():
     print("\n")
